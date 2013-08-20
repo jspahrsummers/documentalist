f96ed685426df57f9f06bbd8eb774ca4d512486b
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-20 02:50:01 -0700

    Move Fixtures into Tests folder

--- Tests/test.hs
+++ Tests/test.hs
@@ -7,8 +7,8 @@ import Text.Documentalist.SourceParser
 
 main :: IO ()
 main = do
-    f_test <- newSourceFile "Fixtures/test_header.h"
-    f_rac <- newSourceFile "Fixtures/RACSignal.h"
+    f_test <- newSourceFile "Tests/Fixtures/test_header.h"
+    f_rac <- newSourceFile "Tests/Fixtures/RACSignal.h"
     p_test <- parse f_test
     p_rac <- parse f_rac
     print p_rac
