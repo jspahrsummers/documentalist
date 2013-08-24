b763d6d8287df997289609e1f2ca3c72247877a6
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-24 13:09:46 -0700

    import/export shortcut as suggested by hlint
    @maxpow4h

--- Text/Documentalist.hs
+++ Text/Documentalist.hs
@@ -1,7 +1,5 @@
-module Text.Documentalist ( module Text.Documentalist.SourceParser
-                          , module Text.Documentalist.CommentParser
-                          , module Text.Documentalist.Writer
-                          ) where
-import Text.Documentalist.SourceParser
-import Text.Documentalist.CommentParser
-import Text.Documentalist.Writer
+module Text.Documentalist ( module D ) where
+
+import Text.Documentalist.SourceParser as D
+import Text.Documentalist.CommentParser as D
+import Text.Documentalist.Writer as D
