53e24c4a46e7863136c431351a52f5775c8e106a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-24 01:31:00 -0700

    Refactor CommentParser typeclass
    No need for it to be a monad — it complicates thing. Use a custom error
    type instead.

--- Text/Documentalist/CommentParser.hs
+++ Text/Documentalist/CommentParser.hs
@@ -42,9 +42,17 @@ data Parameter = Parameter (Declaration ()) [Span]
 newtype Result = Result [Span]
     deriving (Eq, Show)
 
--- | A monad capable of parsing a specific 'Comment' syntax.
-class MonadError e p => CommentParser e p where
+-- | An error that occurred during comment parsing.
+data CommentParseError = CommentParseError
+    { file :: Maybe FilePath
+    , line :: Maybe Integer
+    , message :: String
+    } deriving (Eq, Show)
+
+instance Error CommentParseError where
+    strMsg s = CommentParseError { file = Nothing, line = Nothing, message = s }
+
+-- | Parses a specific 'Comment' syntax.
+class CommentParser p where
     -- | Parses the comments in the given package into 'DocBlock's.
-    --
-    --   Any errors will be indicated using @throwError@.
-    parse :: Package (Maybe Comment) -> p (Package (Maybe DocBlock))
+    parse :: p -> Package (Maybe Comment) -> Either CommentParseError (Package (Maybe DocBlock))
