9074a6dbbab2a3c07ec6f2fe5bcebe14bc357720
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-25 00:14:51 -0700

    Better Show instances for CommentParser types

--- Text/Documentalist/CommentParser.hs
+++ Text/Documentalist/CommentParser.hs
@@ -2,6 +2,9 @@
 module Text.Documentalist.CommentParser where
 
 import Control.Monad.Error.Class
+import Control.Monad.Writer.Lazy
+import Data.List
+import Data.Maybe
 import Text.Documentalist.SourceParser
 
 -- | A documentation block for a declaration.
@@ -11,13 +14,35 @@ data DocBlock = DocBlock
     , parameters :: [DocParam]
     , example :: Maybe Code
     , result :: Maybe Result
-    } deriving (Eq, Show)
+    } deriving Eq
+
+instance Show DocBlock where
+    show doc =
+        let desc = description doc
+            params = parameters doc
+            ex = example doc
+            res = result doc
+        
+            show' :: Writer [String] ()
+            show' = do
+                tell ["# Summary: " ++ show (summary doc)]
+                when (length desc > 0) $ tell ["# Description:\n" ++ show desc ++ "\n"]
+                when (length params > 0) $ tell ["# Parameters:\n" ++ show params ++ "\n"]
+                when (isJust ex) $ tell ["# Example:\n" ++ show (fromJust ex) ++ "\n"]
+                when (isJust res) $ tell ["# Result: " ++ show (fromJust res)]
+        in intercalate "\n" $ execWriter show'
 
 -- | A newline-delimited section of text.
 data Paragraph = TextParagraph [Span]
                | CodeBlock Code
                | QuotedText Paragraph
-               deriving (Eq, Show)
+               deriving Eq
+
+instance Show Paragraph where
+    show (TextParagraph spans) = show spans
+    show (CodeBlock (Code str)) = "```\n" ++ str ++ "\n```"
+    show (QuotedText para) = init $ unlines $ map ((++) "> ") $ lines $ show para
+    showList paras = (++) $ intercalate "\n\n" $ map show paras
 
 -- | Represents a portion of text in a documentation string.
 data Span = PlainText String
@@ -28,7 +53,18 @@ data Span = PlainText String
           | EmphasizedText Span
           | StrongText Span
           | UnderlinedText Span
-          deriving (Eq, Show)
+          deriving Eq
+
+instance Show Span where
+    show (PlainText str) = str
+    show (Reference decl) = "ref:" ++ show decl
+    show (WebLink url) = "link:" ++ url
+    show (InlineImage url) = "img:" ++ url
+    show (InlineCode (Code str)) = "`" ++ str ++ "`"
+    show (EmphasizedText span) = "_" ++ show span ++ "_"
+    show (StrongText span) = "*" ++ show span ++ "*"
+    show (UnderlinedText span) = "_" ++ show span ++ "_"
+    showList spans = (++) $ intercalate " " $ map show spans
 
 -- | A block or span of code in the source language.
 newtype Code = Code String
