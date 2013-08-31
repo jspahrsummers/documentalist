4c20ea226741620871305388a8b14a3c68a62903
Maxwell Swadling <maxwellswadling@gmail.com>
2013-08-31 12:23:32 +1000

    Changed parseDocs' type
    For better error reporting.

--- Main/Main.hs
+++ Main/Main.hs
@@ -1,6 +1,8 @@
 import Control.Applicative
-import Control.Monad
+import Data.Traversable hiding (mapM)
+import Prelude
 import System.Environment
+import System.IO (hPutStrLn, stderr)
 import Text.Documentalist
 import Text.Documentalist.CommentParser.TomDoc
 import Text.Documentalist.SourceParser.Clang
@@ -9,4 +11,11 @@ main :: IO ()
 main = do
     sources <- getArgs
     docs <- runClangParser $ mapM parse sources
-    print $ map (parseDocs TomDocParser) docs
+    let commented = map (parseDocs TomDocParser) docs
+    displayable <- mapM (traverse showErrors) commented
+    print displayable
+
+showErrors :: Either CommentParseException DocBlock -> IO (Maybe DocBlock)
+showErrors (Left e) = hPutStrLn stderr (show e) >> return Nothing
+showErrors (Right x) = return $ Just x
+
