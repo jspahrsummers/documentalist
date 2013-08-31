8282161fc8c43e7ce3cf0bf8cc3bc6dc4294922f
Maxwell Swadling <maxwellswadling@gmail.com>
2013-08-31 11:36:57 +1000

    Put back in Data.Foldable instances

--- Text/Documentalist/Types/Package.hs
+++ Text/Documentalist/Types/Package.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Text.Documentalist.Types.Package ( Identifier(..)
                                         , Type(..)
                                         , SuperTypes
@@ -12,6 +12,8 @@ module Text.Documentalist.Types.Package ( Identifier(..)
                                         , traversePackage
                                         ) where
 
+import Data.Foldable
+import Data.Traversable
 import Text.Documentalist.Util
 
 -- | An identifier, as it would be written in the source language.
@@ -40,13 +42,13 @@ type UnderlyingType = Maybe Type
 -- | Any kind of documentable declaration, associated with data of type @t@.
 data Declaration t = DecLeaf t Identifier DecFLeaf
                    | DecNode t Identifier DecFNode [Declaration t]
-                   deriving (Eq, Show, Functor)
+                   deriving (Eq, Show, Functor, Foldable, Traversable)
 
 -- | A declaration family type for a declaration that may contain more declarations inside of it
 data DecFNode = Class           SuperTypes     -- ^ A class declaration.
               | Interface       SuperTypes     -- ^ An abstract interface, or Objective-C protocol definition.
               | Mixin           Type           -- ^ A mixin, or Objective-C category declaration.
-              | Enumeration     UnderlyingType -- ^ An enumeration.                   
+              | Enumeration     UnderlyingType -- ^ An enumeration.
               | Function        ResultTypes    -- ^ A function.
               | ClassMethod     ResultTypes    -- ^ A class method or static member function.
               | InstanceMethod  ResultTypes    -- ^ An instance method or member function.
@@ -63,7 +65,7 @@ data DecFLeaf = Property        UnderlyingType -- ^ A property declaration in a 
 --
 --   The declaration list should contain any top-level declarations in order of appearance.
 data Module t = Module String [Declaration t]
-    deriving (Eq, Functor)
+    deriving (Eq, Functor, Foldable, Traversable)
 
 instance Eq t => Ord (Module t) where
     compare (Module a _) (Module b _) = compare a b
@@ -73,7 +75,7 @@ instance Show t => Show (Module t) where
 
 -- | A package to treat as a single unit for the purposes of documentation generation.
 data Package t = Package String [Module t]
-    deriving (Eq, Functor)
+    deriving (Eq, Functor, Foldable, Traversable)
 
 instance Eq t => Ord (Package t) where
     compare (Package a _) (Package b _) = compare a b
