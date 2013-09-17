a594d73ee62e83462751e7011d5770c0d8a0a7d3
Maxwell Swadling <maxwellswadling@gmail.com>
2013-09-18 01:14:42 +1000

    added quote examples

--- Tests/TomDocExamples.hs
+++ Tests/TomDocExamples.hs
@@ -11,7 +11,25 @@ whats this
 
 `1 + 1`
 
-~~Nah~~
+```
+let code = good
+in code
+```
+
+> A quote from someone
+> who likes to use computers
+> > i like computers
+> > they gud
+> quote was 4/10
+
+normal mode text
+paragraph
+
+  a bit indented
+  ya kno
+  but not a quote
+  this is ~~fancy~~
+
 
 [wow](http://github.com)
 ![gifs](http://i.imgur.com/vwMin.gif)
