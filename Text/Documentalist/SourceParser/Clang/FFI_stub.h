c4689b649af57cd54d971f163b2c1a8bf9f614fd
Maxwell Swadling <maxwellswadling@gmail.com>
2013-09-26 19:48:28 +1000

    removed tmp file

--- Text/Documentalist/SourceParser/Clang/FFI_stub.h
+++
@@ -1,9 +0,0 @@
-#include "HsFFI.h"
-#ifdef __cplusplus
-extern "C" {
-#endif
-extern HsInt32 TextziDocumentalistziSourceParserziClangziFFI_d20S(StgStablePtr the_stableptr, HsPtr a1, HsPtr a2);
-#ifdef __cplusplus
-}
-#endif
-
