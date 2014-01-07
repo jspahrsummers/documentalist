78e4b109883f72a646c46bc76ac1813fd40d6965
Maxwell Swadling <maxwellswadling@gmail.com>
2014-01-07 22:48:26 +1100

    renamed showList__

--- Text/Documentalist/PrettyPrint.hs
+++ Text/Documentalist/PrettyPrint.hs
@@ -3,13 +3,13 @@ module Text.Documentalist.PrettyPrint (PrettyPrint(..)) where
 class PrettyPrint a where
     pprint :: a -> String
     pprintList :: [a] -> String -> String
-    pprintList = showList__ (\x -> (++) (pprint x))
+    pprintList = showList' (\x -> (++) (pprint x))
 
 -- from GHC's Text.Show. (c) The University of Glasgow 2001
 -- License: BSD-style (see the file libraries/base/LICENSE)
-showList__ :: (a -> String -> String) -> [a] -> String -> String
-showList__ _     []     s = "[]" ++ s
-showList__ showx (x:xs) s = '[' : showx x (showl xs)
+showList' :: (a -> String -> String) -> [a] -> String -> String
+showList' _     []     s = "[]" ++ s
+showList' showx (x:xs) s = '[' : showx x (showl xs)
     where
       showl []     = ']' : s
       showl (y:ys) = ',' : showx y (showl ys)
